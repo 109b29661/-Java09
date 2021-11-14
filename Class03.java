74405d67d5385c263da3fc6ac973deecb21e61a5
109b29661 <91444896+109b29661@users.noreply.github.com>
2021-11-14 16:28:15 +0800

    Update and rename Class01(b).java to Class03.java

---
+++ Class03.java
@@ -0,0 +1,20 @@
+package java09;
+class Caaa{           //不可宣告為public，一個文件中只有與文件名稱相同的類別才能宣告為public
+	                     
+	private int value;//改成public，其他類別也能存取此變數，對執行此程式沒影響
+	public Caaa() {
+		value = 10;
+		System.out.println("value="+value);
+	}
+	public Caaa(int i) {
+		value=i;
+		System.out.println("value="+value);
+	}
+}
+public class note9001 
+{
+	public static void main(String args[]) {
+    Caaa obj1 = new Caaa();//呼叫public Caaa()
+    Caaa obj2 = new Caaa(12);//public Caaa(int i)
+}
+}
