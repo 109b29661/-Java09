df744b0610621f0b896cb57da5ff8a0fd765c086
109b29661 <91444896+109b29661@users.noreply.github.com>
2021-11-14 15:01:10 +0800

    Update and rename Class01(a).java to Class01(c).java

---
+++ Class01(c).java
@@ -0,0 +1,16 @@
+class CRectangle{
+	int width;
+	int  height;
+CRectangle(int w,int h){
+	w = width;
+	h = height;
+	}
+CRectangle(){
+        //width = 10;
+        //height = 8;
+	this(10,8);
+	}
+}
+public class Class01 {
+CRectangle a = new CRectangle();
+}
